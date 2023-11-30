b99fc7b37ad4d57509790123d9399c08c8b563fa
David De La Rosa <daviddelarosa@Davids-MacBook-Pro.local>
2023-11-30 14:49:52 -0600

    updates to connections file

---
+++ public/js/script.js
@@ -0,0 +1,8 @@
+// public/js/script.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Script loaded!'); // Example: Log a message to the console when the page is loaded
+  
+    // Your JavaScript code goes here
+  });
+  
